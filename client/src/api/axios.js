100a524123b6684372a8c76f2cfe69452549cb44
shrikant <shrikantswami2005@gmail.com>
2026-08-29 14:48:20 +0530

    Fix: hardcode API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://tripvault-omrt.onrender.com/api'
 });
 
-// Automatically attach JWT token to every request
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
